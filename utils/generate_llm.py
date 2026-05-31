66c7388557e62edc58749388167c136a45643342
ander <anderon.bringhenti@bradesco.com.br>
2026-05-31 19:38:02 -0300

    aqui

--- utils/generate_llm.py
+++ utils/generate_llm.py
@@ -16,18 +16,29 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 def generate_gpt(prompt, llm_parameters: Dict):
     model = llm_parameters.get("model", "gpt-5.4-mini")
     json_mode = llm_parameters.get("json_mode", False)
-
-    request_args = {
-        "model": model,
-        "input": prompt,
-    }
+    temperature = llm_parameters.get("temperature", 0)
+    seed = llm_parameters.get("seed", None)
+    instructions = llm_parameters.get("instructions", None)
 
     if json_mode:
-        request_args["text"] = {"format": {"type": "json_object"}}
+        text = {"format": {"type": "json_object"}}
     else:
-        request_args["text"] = {"format": {"type": "text"}}
+        text = {"format": {"type": "text"}}
 
-    response = client.responses.create(**request_args)
+    response = client.responses.create(
+        model=model,
+        input=prompt,
+        text=text,
+        temperature=temperature,
+        )
+
     return response.output_text
 
+
+def get_embeddings(texts, model="text-embedding-3-large"):
+    response = client.embeddings.create(
+        model=model,
+        input=texts
+    )
+    return response.data[0].embedding
 
